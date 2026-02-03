228332db0c8c6c9295705af80aa8491513b11597
Sakshi Patil <sakshipatil1345@gmail.com>
2026-02-03 13:56:43 -0500

    Implemented delete book review for authenticated users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -58,9 +58,26 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
   // Add or update review
   books[isbn].reviews[username] = review;
-
   return res.status(200).json({ message: "Review added/updated successfully" });
 });
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization?.username;
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  };
+  if (!books[isbn].reviews[username]) {
+    return res.status(404).json({ message: "No review found for this user" });
+  };
+
+  // Delete the user's review
+  delete books[isbn].reviews[username];
+  return res.status(200).json({ message: "Review deleted successfully" });
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
